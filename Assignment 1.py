7610348c02de535448a9baf389532143eb42ca97
dpk-bhat <ddbhat99@gmail.com>
2022-04-10 16:12:13 +0530

    make linked list global

--- Assignment 1.py
+++ Assignment 1.py
@@ -9,6 +9,7 @@ def main():
     Task.task_number will contain a unique ID
     Task.is_complete will indicate if the task is marked as complete or not
     """
+
     class Task:
         def __init__(self, task_string, task_number="") -> None:
             nonlocal task_int
@@ -40,6 +41,7 @@ def main():
     Node.data will have a Task object
     Node.next will have None, which will be replaced by address of next Task
     """
+
     class Node:
         def __init__(self, data, next=None) -> None:
             self.data = data
@@ -56,8 +58,8 @@ def main():
     LinkedList.count will contain the number of nodes presenet in the linked list
     This class provides funtion for basic funtionalities i.e. append, find and remove
     """
-    class LinkedList:
 
+    class LinkedList:
         def __init__(self) -> None:
             self.head = None
             self.tail = None
@@ -75,11 +77,13 @@ def main():
         def append(self, task):
             node = Node(task, None)
             try:
+                
                 """
                 Assign the node as head of linked list if linked list is empty
                 Else append the node to tail
                 return the newly appended task
                 """
+
                 if self.is_empty():
                     self.head = node
                     self.tail = node
@@ -94,21 +98,25 @@ def main():
         def remove(self, task):
             temp = self.head
             try:
+
                 """
                 If the linked list is empty log the error and return nothing
                 Else search and remove the task from the linked list
                 """
+
                 if temp == None:
                     log_error("LINKED LIST IS EMPTY")
                 else:
+
                     """
                     If the head is the task to be removed, remove task and reassign head to next node
                     Else search and remove the task from the llinked list
                     Return the removed task
                     """
+                    
                     if temp.data == task:
                         self.head = self.head.next
-                        self.count -=1
+                        self.count -= 1
                         return temp.data
                     else:
                         while temp.next != None:
@@ -124,28 +132,33 @@ def main():
             except:
                 print("Something went wrong during Remove")
 
-        def find(self, task):
+        def search(self, task):
             temp = self.head
             found_tasks = []
             try:
+
                 """
                 If the linked list is empty log the error and return nothing
                 Else search for the task in the linked list
                 Return list of tasks found
                 """
+
                 if temp == None:
                     log_error("LINKED LIST IS EMPTY")
                 else:
                     while temp != None:
                         # If the search string is present in any of the task, append it to the list of found task
-                        if task.task_string in temp.data.task_string.lower().strip().strip(".") :
+                        if (
+                            task.task_string
+                            in temp.data.task_string.lower().strip().strip(".")
+                        ):
                             found_tasks.append(temp.data)
                         temp = temp.next
                 return found_tasks
             except:
-                print("Something went wrong during Find")
+                print("Something went wrong during search")
 
-    def addTask(linked_list, task_string=""):
+    def addTask(task_string=""):
         # Check if task_string is not null/empty
         if task_string:
             # Create a task and append it to the linked list and write in output file
@@ -155,7 +168,7 @@ def main():
         else:
             log_error("TASK STRING IS EMPTY")
 
-    def removeTask(linked_list, task_string="", task_number=""):
+    def removeTask(task_string="", task_number=""):
         # Check if the task_string or task_number has some value
         if task_string or task_number:
             task_to_remove = Task(task_string, task_number)
@@ -163,25 +176,32 @@ def main():
             # Check if removed_task has a task, and if yes, write in the output file
             if removed_task:
                 output_file.write(
-                    "REMOVED:" + removed_task.task_number + "-" + removed_task.task_string
+                    "REMOVED:"
+                    + removed_task.task_number
+                    + "-"
+                    + removed_task.task_string
                 )
             else:
-                output_file.write("TASK :" + task_to_remove.task_string.strip() + " WAS NOT FOUND\n")
+                output_file.write(
+                    "TASK :" + task_to_remove.task_string.strip() + " WAS NOT FOUND\n"
+                )
         else:
             log_error("TASK STRING AND TASK NUMBER IS EMPTY")
 
-    def searchTask(linked_list, search_string=""):
+    def searchTask(search_string=""):
         # Check if the search_string and linked list is not empty
         if search_string and not linked_list.is_empty():
             task_to_find = Task(search_string.strip().strip("."))
             output_file.write("SEARCHED:" + search_string + "-" * 60 + "\n")
-            found_tasks = linked_list.find(task_to_find)
+            found_tasks = linked_list.search(task_to_find)
+
             """
             Check if found_tasks list has any object
             If yes, write it's elements to output file
             Else write the same in output file
             """
-            if found_tasks != None and found_tasks!= []:
+
+            if found_tasks != None and found_tasks != []:
                 for task in found_tasks:
                     output_file.write(task.task_number + " " + task.task_string)
             else:
@@ -190,7 +210,7 @@ def main():
         else:
             log_error("SEARCH STRING OR LINKED LIST IS EMPTY")
 
-    def completeTask(linked_list, task_string="", task_number=""):
+    def completeTask(task_string="", task_number=""):
         # Check if the linked list is empty
         if linked_list.is_empty():
             log_error("LINKED LIST IS EMPTY.")
@@ -201,11 +221,13 @@ def main():
                 found = False
                 temp = linked_list.get_head()
                 task_to_complete = Task(task_string, task_number)
+            
                 """
                 Check the linked list for the task
                 If found, mark as complete and write to output file
                 Else write the same in output file
                 """
+
                 while temp.next != None:
                     if task_to_complete == temp.data:
                         found = True
@@ -215,13 +237,15 @@ def main():
                         )
                     temp = temp.next
                 if not found:
-                    log_error(f"CANNOT MARK COMPLETE: TASK {task_to_complete.task_string.strip()} NOT FOUND")
+                    log_error(
+                        f"CANNOT MARK COMPLETE: TASK {task_to_complete.task_string.strip()} NOT FOUND"
+                    )
             else:
                 log_error("TASK STRING AND TASK NUMBER IS EMPTY")
 
-    def incompleteTask(linked_list, task_string="", task_number=""):
+    def incompleteTask(task_string="", task_number=""):
         # Check if the linked list is empty
-        if linked_list.is_empty() == None:
+        if linked_list.is_empty():
             log_error("LINKED LIST IS EMPTY.")
             return None
         else:
@@ -230,11 +254,13 @@ def main():
                 found = False
                 temp = linked_list.get_head()
                 task_to_incomplete = Task(task_string, task_number)
+                
                 """
                 Check the linked list for the task
                 If found, mark as complete and write to output file
                 Else write the same in output file
                 """
+
                 while temp.next != None:
                     if task_to_incomplete == temp.data:
                         found = True
@@ -244,13 +270,15 @@ def main():
                         )
                     temp = temp.next
                 if not found:
-                    log_error(f"CANNOT MARK INCOMPLETE TASK {task_to_incomplete.task_string.strip()} NOT FOUND")
+                    log_error(
+                        f"CANNOT MARK INCOMPLETE TASK {task_to_incomplete.task_string.strip()} NOT FOUND"
+                    )
             else:
                 log_error("TASK STRING AND TASK NUMBER IS EMPTY")
 
     def statusTask(linked_list):
-        if linked_list.is_empty() == None:
-            log_error
+        if linked_list.is_empty():
+            log_error("LINKED LIST IS EMPTY.")
         # Append to file
         output_file.write(
             "Task-Number".ljust(15)
@@ -274,7 +302,7 @@ def main():
 
     def log_error(error_message):
         # Append appropriate error message to the output file
-        output_file.write(error_message+ "\n")
+        output_file.write(error_message + "\n")
 
     """
     Function for debuggging
@@ -292,30 +320,31 @@ def main():
         file = open(read_input_file, "r")
         lines = file.readlines()
         # Initializing empty linked list
-        linked_list = LinkedList()
         for line in lines:
             # Perform operation depending in instruction
             instruction, task_string = line.split(":")
             instruction = instruction.strip().lower()
-
             if instruction == "add a task":
-                addTask(linked_list, task_string)
+                addTask(task_string)
             elif instruction == "remove task":
-                removeTask(linked_list, task_string)
+                removeTask(task_string)
             elif instruction == "search task":
-                searchTask(linked_list, task_string)
+                searchTask(task_string)
             elif instruction == "mark complete":
-                completeTask(linked_list, task_string)
+                completeTask(task_string)
             elif instruction == "mark incomplete":
-                incompleteTask(linked_list, task_string)
+                incompleteTask(task_string)
             elif instruction == "task status":
                 statusTask(linked_list)
             else:
                 log_error("INVALID INSTRUCTION")
+            
             """
             Printing linked list after each operation for debugging
             print_linked_list(linked_list)
             """
+
+    linked_list = LinkedList()
     initiateToDoList("inputPS5.txt")
 
     # Close the output file after writing
